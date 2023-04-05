8854b8c6c82520ad39d5fc5a6675887ee7b659d3
Matthew Clark <afromatt6288@hotmail.com>
2023-04-05 11:36:01 -0600

    Lab Complete

--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True) ##not always
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'''
+        id: {self.id}
+        name: {self.name}
+        image: {self.image}
+        price: {self.price}
+        '''
